b7c25da5c682724238b06d056f0b3fb2b2088813
Hit246 <hitarth0236@gmail.com>
2025-10-21 14:39:56 +0530

    add

--- Level 3/Task1/backend/core/settings.py
+++ Level 3/Task1/backend/core/settings.py
@@ -60,6 +60,10 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "https://django-blog-demo.netlify.app",
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
